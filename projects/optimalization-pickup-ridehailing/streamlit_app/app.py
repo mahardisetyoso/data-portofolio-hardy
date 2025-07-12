d5ee07bbdbbc45a67d8a35d17e9439802a7f59f3
masmahardi <64582990+mahardisetyoso@users.noreply.github.com>
2025-07-12 21:42:52 +0700

    week 1 commit 🚀

--- projects/optimalization-pickup-ridehailing/streamlit_app/app.py
+++ projects/optimalization-pickup-ridehailing/streamlit_app/app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+from utils.db import engine
 
+st.title("Tes Koneksi Supabase")
+
+try:
+    with engine.connect() as conn:
+        df = pd.read_sql("SELECT NOW()", conn)
+        st.success(f"Koneksi berhasil! Waktu server: {df.iloc[0,0]}")
+except Exception as e:
+    st.error(f"Gagal koneksi: {e}")
